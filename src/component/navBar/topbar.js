febd5ca5df73c682947c2a477a9062acae021b3c
Eemayas <prashantmanandhar2002@gmail.com>
2023-04-05 21:31:17 +0545

    r

--- src/component/navBar/topbar.js
+++ src/component/navBar/topbar.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import './topbar.css';
-
+import Avatar1 from "./../../Images/avatar.png";
+import { Avatar } from "@material-ui/core";
+import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
+import AddIcon from '@mui/icons-material/Add';
+import MainLogo from "./../../Images/MainLogo.png"
 class Topbar extends React.Component {
     render(){
         return(
             <div class = 'dashboardTopbar'>
-                <h1>Project Hub</h1>
+            <img className="TopbarLogo" alt="  Project Hub" src={MainLogo} />
+              
                 <div class = 'dashboardEndbutton'>
-                    <p>Home</p>
-                    <p>Home</p>
+                  <AddIcon />
+                    <NotificationsActiveIcon />
+                    <Avatar className="avatarItems" src= {Avatar1}/>
                 </div>
             </div>
         )
--- src/pages/progress.jsx
+++ src/pages/progress.jsx
@@ -6,24 +6,23 @@ import MemberListBox from "../component/memberListBox/memberListBox";
 import { Topbar } from "../component/navBar/topbar";
 const ProgressPage = () => {
   return (
-    <div>  <Topbar />
+    <div>
+      <Topbar />
       <div className="progressContainer">
-  
-      <div className="leftCointainer">
-        <TopProgressBar />
-        <div className="columns" >
-          <ColumnProgessBox title={"BackLog"} />
-          <ColumnProgessBox title={"To Do"} />
-          <ColumnProgessBox title={"In Progress"} />
-          <ColumnProgessBox title={"Review"} />
+        <div className="leftCointainer">
+          <TopProgressBar />
+          <div className="columns">
+            <ColumnProgessBox title={"BackLog"} />
+            <ColumnProgessBox title={"To Do"} />
+            <ColumnProgessBox title={"In Progress"} />
+            <ColumnProgessBox title={"Review"} />
+          </div>
+        </div>
+        <div className="rightCointainer">
+          <MemberListBox />
         </div>
       </div>
-      <div className="rightCointainer">
-    <MemberListBox />
-      </div>
     </div>
-    </div>
-    
   );
 };
 
